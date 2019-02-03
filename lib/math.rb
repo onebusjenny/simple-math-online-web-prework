744a24da2c182ef4d5af0b4ec6dcb4676ee94e17
Jenny Chu <jchudesign0930@gmail.com>
2019-02-03 01:01:03 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,17 @@
 def addition(num1, num2)
-  return 1+1
+  return 6+3
 end
 
 def subtraction(num1, num2)
-  return 2-1
+  return 10-5
 end
 
 def division(num1, num2)
-  return 4/2
+  return 50/2
 end
 
 def multiplication(num1, num2)
-  return 2*2
+  return 60*2
 end
 
 def modulo(num1, num2)
@@ -19,7 +19,7 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-  Retrun Math.sqrt(9)
+  Retrun Math.sqrt(2)
 end
 
 def order_of_operation(num1, num2, num3, num4)
