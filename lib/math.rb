8f45a447d9df7521daf3c12917ab5fc66b81fc5e
Jenny Chu <jchudesign0930@gmail.com>
2019-02-03 01:01:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,9 +19,9 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-  Retrun Math.sqrt(2)
+  Retrun Math.sqrt(16)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-   return 11 + (59/(13*8))
+   return 16 + (24/(12*1))
 end
